c440b3efe6a1d4cac4c41e6b4ab9789029f2ccdc
Tovieye Moses Ozi <tovieye.ozi@gmail.com>
2019-09-29 18:02:44 +0100

    deploy

--- project.clj
+++ project.clj
@@ -3,10 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ring "1.7.1"]
-                 [compojure "1.6.1"]]
- :repl-options {:init-ns todo-list.core}
+  :dependencies [
+                  [org.clojure/clojure "1.10.1"]
+                  [ring "1.7.1"]
+                  [compojure "1.6.1"]
+                  [org.clojure/core.incubator "0.1.4"]]
+  :repl-options {:init-ns todo-list.core}
   :main todo-list.core
   :profiles {:dev 
-                  {:main todo-list.core/-dev-main}})
+                  {:main todo-list.core/-dev-main}}
+  :min-lein-version "2.0.0"
+  :uberjar-name "todo-list.jar")
--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -2,33 +2,78 @@
  (:require [ring.adapter.jetty :as jetty]
           [ring.middleware.reload :refer [wrap-reload]]
           [compojure.core :refer [defroutes GET]]
-          [compojure.route :refer [not-found]]))
+          [compojure.route :refer [not-found]]
+          [compojure.coercions :refer [as-int]]
+          [ring.handler.dump :refer [handle-dump]]
+          [clojure.core.strint :refer [<<]]))
 
 (defn welcome
-  "A ring handler to process all requests sent to the webapp"
+  "A ring handler to respond with a simple welcome message"
   [request]
- (if (= "/" (:uri request))
-    { :status 200
-      :body
-          "<h1>Hello, Clojure World</h1>
-            <p>Welcome to your first Clojure app, I now update automatically<p>"
-      :headers {}}
-    {:status 404
-      :body "<h1>This is not the page you are looking for</h1>
-            <p>Sorry, the page you requested was not found!</p>"
-      :headers {}}))
+  { :status 200
+    :body
+        "<h1>Hello, Clojure World</h1>
+        <p>Welcome to your first Clojure app, I now update automatically<p>
+        <p>I now use defroutes to manage incoming requests</p>"
+    :headers {}})
 
-(defroutes myapp
-    (GET "/" [] "Show something"))
+(defn goodbye
+  "A song to wish you goodbye"
+  [request]
+  {:status 200
+   :body "<h1>Walking back to happiness</h1>
+          <p>Walking back to happiness with you</p>
+          <p>Said, Farewell to loneliness I knew</p>
+          <p>Laid aside foolish pride</p>
+          <p>Learnt the truth from tears I cried</p>"
+   :headers {}})
+
+(defn about
+  "this is a page about the website"
+  [request]
+  {:status 200
+    :body "<h1>My way if you wanna take it there</h1>"
+    :headers {}})
+
+(defn hello
+  "A customized hello message"
+  [name]
+  {:status 200
+    :body (<< "<h1>Hello ~{name}. I got your name from the web URL</h1>")
+    :headers {}})
+
+(def operands {"+" + "-" - "*" * ":" /})
+
+(defn calculator
+  "A very simple calculator that can add, divide, subtract and multiply.  This is done through the magic of variable path elements."
+  [a b op]
+  (let [f  (get operands op)]
+    (if f
+      {:status 200
+       :body (str "Calculated result: " (f a b))
+       :headers {}}
+      {:status 404
+       :body "Sorry, unknown operator.  I only recognise + - * : (: is for division)"
+       :headers {}})))
+
+(defroutes app
+    (GET "/" [] welcome)
+    (GET "/goodbye" [] goodbye)
+    (GET "/about" [] about)
+    (GET "/hello/:name" [name] (hello name))
+    (GET "/calculator/:a/:op/:b" [a :<< as-int b :<< as-int op] (calculator a b op))
+    (GET "/request-info" [] handle-dump)
+    (not-found "<h1>This is not the page you are looking for</h1>
+                  <p>Sorry, the page you requested was not found!</p>"))
 
 (defn -dev-main
   "A very simple web server using Ring and Jetty  that reloads code changes via the development profile of Leiningen"
   [port-number]
-  (jetty/run-jetty (wrap-reload #'welcome)
+  (jetty/run-jetty (wrap-reload #'app)
                    {:port (Integer. port-number)}))()
 
 (defn -main
   "A very simple web server using Ring and Jetty"
   [port-number]
-  (jetty/run-jetty welcome
+  (jetty/run-jetty app
     {:port (Integer. port-number)}))
